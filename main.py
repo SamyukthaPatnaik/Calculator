ef63ff8a45fb1927550bbf2b6be0d01b95cbf9b1
Samyuktha Patnaik <samyukthapatnaik17@gmail.com>
2023-01-30 13:58:06 +0530

    calculator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime, date
 import dateutil.utils
 from streamlit_option_menu import option_menu
 from forex_python.converter import CurrencyRates
@@ -37,23 +38,52 @@ def discount():
         st.success ( saved )
 
 def age():
-    st.title("Age Calculator")
-    birth_date = st.date_input("Your Date Of Birth")
-    today = dateutil.utils.today()
-    age = today.year - birth_date.year - ((today.month, today.day)<(birth_date.month, birth_date.day))
-    st.subheader("Age -")
-    st.success(age)
+    # st.title("Age Calculator")
+    # birth_date = st.date_input("Enter your birthdate")
+    # today = dateutil.utils.today()
+    # age = today.year - birth_date.year - ((today.month, today.day)<(birth_date.month, birth_date.day))
+    #
+    # st.subheader("Age -")
+    # st.success(age)
+    def calculate_age(born):
+        today = date.today ()
+        return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+    def exact_age(born):
+        today = datetime.now ()
+        years_diff = today.year - born.year
+        months_diff = today.month - born.month
+        days_diff = today.day - born.day
+        if days_diff < 0:
+            months_diff -= 1
+        if months_diff < 0:
+            months_diff += 12
+            years_diff -= 1
+        return years_diff, months_diff, days_diff
+
+    def main():
+        st.title ( "Age Calculator" )
+        born = st.date_input ( "Enter your birthdate (YYYY-MM-DD):" )
+        if st.button ( "Calculate" ):
+            age_years = calculate_age ( born )
+            exact_age_years, exact_age_months, exact_age_days = exact_age ( born )
+            st.write ( f"You are {age_years} years, {exact_age_months} months, and {exact_age_days} days old" )
+
+    if __name__ == '__main__':
+        main ()
 
 def loan():
     st.title("Loan Calculator")
-
     amount = st.number_input ( "Loan Amount" )
     rate = st.number_input ( "Interest Rate (%)" )
     years = st.number_input ( "Loan Term (years)" )
     r = rate / 100 / 12
     n = years * 12
     payment = (r * amount) / (1 - ((1 + r) ** (-n)))
-    st.write ( "Monthly Payment: ", payment )
+    st.write ( "Monthly EMI: ", payment )
+    # interest = (amount * rate * years) / 100
+    # total = interest * rate
+    # st.write("Total Interest Amount ", total)
 
 def currency():
     st.title("Currency Calculator")
